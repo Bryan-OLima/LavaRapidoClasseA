2966d6866ae83a4db09e4b23eec2579a3757e246
Bryan Lima <bryanosoriolima@gmail.com>
2026-06-11 15:34:59 -0300

    search working

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import WashCard from '../../components/WashCard/WashCard';
 import { MOCK_WASHES } from '../../mocks/washes';
@@ -7,11 +7,12 @@ import './style.css';
 
 function HomePage() {
   const [list, setList] = useState(MOCK_WASHES);
+  const [search, setSearch] = useState('');
   const handleRemoveWash = (id: string) => {
     setList((p) => p.filter((wash) => wash.id !== id));
   };
 
-  const washList = list.map((wash) => (
+  const washCardList = list.map((wash) => (
     <WashCard
       key={wash.id}
       id={wash.id}
@@ -25,10 +26,34 @@ function HomePage() {
     />
   ));
 
+  const washSearch = list.filter((wash) => {
+    return wash.car.plate
+      .toLocaleLowerCase()
+      .includes(search.toLocaleLowerCase());
+  });
+
   return (
     <section className="page-section">
-      <input type="text" className="searchbar" placeholder="Buscar por Placa" />
-      {washList}
+      <input
+        type="text"
+        className="searchbar"
+        placeholder="Buscar por Placa"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {/* {washCardList} */}
+      {washSearch.map((wash) => (
+        <WashCard
+          key={wash.id}
+          id={wash.id}
+          car={wash.car.model}
+          plate={wash.car.plate}
+          client={wash.client.name}
+          value={wash.service.value}
+          exit={wash.timestamps.exit}
+          notes={`${wash.service.os}. ${wash.service.obs}`}
+          onRemove={handleRemoveWash}
+        />
+      ))}
     </section>
   );
 }
