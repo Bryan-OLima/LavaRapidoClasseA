5985c06df2dfd27dd1d9641ff00d9e097bc562b2
Bryan Lima <bryanosoriolima@gmail.com>
2026-06-12 05:47:04 -0300

    Wash Service and Currency formater added

---
+++ src/services/WashService.ts
@@ -0,0 +1,42 @@
+import type { Wash } from '../interfaces/Washes';
+
+export default function WashService() {
+  console.log('local storage service');
+
+  return {
+    set: (wash: Wash) => {
+      localStorage.setItem(wash.id, JSON.stringify(wash));
+      console.log('registrado');
+    },
+
+    get: (id: string) => {
+      const washString = localStorage.getItem(id);
+      if (!washString) return null;
+
+      return JSON.parse(washString) as Wash;
+    },
+
+    delete: (id: string) => {
+      localStorage.removeItem(id);
+    },
+
+    getAll: () => {
+      const db: any = {};
+
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key?.startsWith('Wash_')) {
+          const value = localStorage.getItem(key);
+
+          if (!value) return null;
+          try {
+            db[key] = JSON.parse(value);
+          } catch {
+            db[key] = value;
+          }
+        }
+      }
+      return db;
+    },
+  };
+}
