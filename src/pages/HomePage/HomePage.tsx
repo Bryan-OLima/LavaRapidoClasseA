903c929df08795b18ea575a5e1ade32c6d46ce74
Bryan Lima <bryanosoriolima@gmail.com>
2026-06-16 11:26:41 -0300

    HomePage styles edited

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -28,17 +28,19 @@ function HomePage() {
   });
 
   return (
-    <section className="page-section">
-      <input
-        type="text"
-        className="searchbar"
-        placeholder="Buscar por Placa"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      {washSearch?.map((wash: Wash) => (
-        <WashCard key={wash.id} wash={wash} />
-      ))}
-    </section>
+    <>
+      <section className="page-section">
+        <input
+          type="text"
+          className="searchbar"
+          placeholder="Buscar por Placa"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {washSearch?.map((wash: Wash) => (
+          <WashCard key={wash.id} wash={wash} />
+        ))}
+      </section>
+    </>
   );
 }
 
